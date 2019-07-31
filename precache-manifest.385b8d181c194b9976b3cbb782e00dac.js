d769b06a820c6e1a0b339a4c16b7eefaa350d9b3
ashwin <ashwinvemula9@gmail.com>
2019-08-01 03:36:11 +0530

    Initial Commit

---
+++ precache-manifest.385b8d181c194b9976b3cbb782e00dac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "adf2c2e270d8670dbf03",
+    "url": "/react-ashwin/static/js/runtime~main.adf2c2e2.js"
+  },
+  {
+    "revision": "80bb774fdd91ed7bcef0",
+    "url": "/react-ashwin/static/js/main.80bb774f.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react-ashwin/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "80bb774fdd91ed7bcef0",
+    "url": "/react-ashwin/static/css/main.9f3deafe.chunk.css"
+  },
+  {
+    "revision": "61ba3de0b145c96ccf0deb9c2d9c4cdd",
+    "url": "/react-ashwin/index.html"
+  }
+];
